000bfb9b2a5325ea32e06a87384382f7dd5e3041
msatra2002 <76219469+msatra2002@users.noreply.github.com>
2024-05-14 23:14:49 +0000

    Updated index page

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
+<!DOCTYPE html>
 <html>
   <head>
-    <title>PHP Test</title>
+    <title>Mohamed</title>
   </head>
+  
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
-
+  
+    <h1>Assignment 1</h1>
+    
+  
+  </body>
 </html>
