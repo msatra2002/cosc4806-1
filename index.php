de8628ade7875dba0a6628cb5c234bc2074caabb
msatra2002 <76219469+msatra2002@users.noreply.github.com>
2024-05-15 01:10:04 +0000

    Added the authentication

--- index.php
+++ index.php
@@ -3,6 +3,15 @@ session_start();
 
 // Check if user is authenticate
 // if NOT, send them to login.php ... header()...
+if (isset($_SESSION['username'])) {
+    // User is logged in
+    echo "You are logged in as " . $_SESSION['username'];
+} else {
+    // User is not logged in
+    echo "You are not logged in";
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -19,5 +28,10 @@ session_start();
 
   
   </body>
+
+  <footer> 
+    <p><a href="/logout.php"> Click here to log out </a></p>
+
+  </footer>
 </html>
 
